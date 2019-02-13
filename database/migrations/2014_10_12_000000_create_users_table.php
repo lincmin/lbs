e1481fafb70db9175d5070287b85b0573861a21f
lincmin <lincm69696@qq.com>
2019-02-13 19:00:58 +0800

    add github Laravel Socialite

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('provider');//存放 OAuth 服务提供方
+            $table->string('provider_id');//存放从第三方 OAuth 那里获取的用户唯一ID
+            $table->text('avatar');//存放从第三方 OAuth 获取的用户头像
             $table->rememberToken();
             $table->timestamps();
         });
