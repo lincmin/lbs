bfaab978b681c0defa0a36c5d4fc92677143496c
lincmin <lincm69696@qq.com>
2019-02-18 14:54:40 +0800

    use vuex in vue page

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -4,12 +4,29 @@
 
 <template>
   <div>
-
+    <ul>
+      <li v-for="cafe in cafes">{{ cafe.name }}</li>
+    </ul>
+    <span v-show="cafesLoadStatus == 1">加载中...</span>
+    <span v-show="cafesLoadStatus == 2">数据加载成功！</span>
+    <span v-show="cafesLoadStatus == 3">数据加载失败！</span>
   </div>
 </template>
 
 <script>
 export default {
-
+  create() {
+    this.$store.dispatch('loadCafes');
+  },
+  computed: {
+    //获取cafes加载状态
+    cafesLoadStatus() {
+      return this.$store.getters.getCafesLoadStatus;
+    },
+    //获取cafes
+    cafes() {
+      return this.$store.getters.getCafes;
+    }
+  }
 }
 </script>
