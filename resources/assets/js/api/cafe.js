4af0a1ee5d55038cfebc2428d993481a9fc81741
lincmin <lincm69696@qq.com>
2019-04-02 17:18:14 +0800

    表单一次提交多个咖啡店位置信息

--- resources/assets/js/api/cafe.js
+++ resources/assets/js/api/cafe.js
@@ -1,28 +1,28 @@
-import {LBS_CONFIG} from '../config.js';
+import { LBS_CONFIG } from '../config.js';
 
 export default {
-     /**
-     * GET /api/v1/cafes
-     */
-    getCafes:function(){
+    /**
+    * GET /api/v1/cafes
+    */
+    getCafes: function () {
         return axios.get(LBS_CONFIG.API_URL + '/cafes');
     },
     /**
      * GET /api/v1/cafes/{cafeID}
      */
-    getCafe: function( cafeID ){
-        return axios.get( LBS_CONFIG.API_URL + '/cafes/' + cafeID );
+    getCafe: function (cafeID) {
+        return axios.get(LBS_CONFIG.API_URL + '/cafes/' + cafeID);
     },
     /**
     * POST /api/v1/cafes
     */
-    postAddNewCafe:function(name,address,city,state,zip){
-        return axios.post(LBS_CONFIG.API_URL + '/cafes',{
+    postAddNewCafe: function (name, locations, website, description, roaster) {
+        return axios.post(LBS_CONFIG.API_URL + '/cafes', {
             name: name,
-            address: address,
-            city: city,
-            state: state,
-            zip: zip
+            locations: locations,
+            website: website,
+            description: description,
+            roaster: roaster
         })
     }
 }
